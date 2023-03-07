39412e26e7d11604d21e06dbac1975fb7253f0d0
Sfd Jucide <sfd@DESKTOP-GHQILUA.localdomain>
2023-03-07 23:44:52 +0300

    add first project BashUtilities

---
+++ C3_SimpleBashUtils/src/cat/cat.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 1000
+
+void clean(char *str);
+void cat(char *file_name, char *flags[10], int flags_count);
+void get_flags(char *flags[10], int flags_count, int *e, int *n, int *b, int *t, int *s,
+               int *v);
+void get_args(int argc, char *argv[argc], char *flags[10], char *files[100],
+              int *flags_counter, int *files_counter);
+void squeeze_blank(char *file_out, int *empty_flag);
+void replace_char(char *file_out, char symb);
+void add_numering(char *file_out, int *str_num, char mode);
+void show_nonprinting(char *file_out);
+
+int main(int argc, char *argv[])
+{
+  char *flags[10];
+  char *files[100];
+  int flags_counter = 0;
+  int files_counter = 0;
+  get_args(argc, argv, flags, files, &flags_counter, &files_counter);
+
+  for (int i = 0; i < files_counter; i++) {
+    cat(files[i], flags, flags_counter);
+  }
+  return 0;
+}
+
+void cat(char *file_name, char *flags[10], int flags_count) {
+  int e = 0;
+  int n = 0;
+  int b = 0;
+  int t = 0;
+  int s = 0;
+  int v = 0;
+  int empty_flag = 0;
+  int line_num = 0;
+  char file_out[N] = {'\0'};
+  FILE *cat_file = fopen(file_name, "r");
+
+  get_flags(flags, flags_count, &e, &n, &b, &t, &s, &v);
+
+  if (cat_file) {
+    while (!feof(cat_file)) {
+      fgets(file_out, 1000, cat_file);
+      if (file_out[0] != '\0') {
+        if (s) {
+          squeeze_blank(file_out, &empty_flag);
+        }
+        if (t) {
+          replace_char(file_out, '\t');
+        }
+        if ((n || b) && empty_flag < 2) {
+          if (b) {
+            add_numering(file_out, &line_num, 'b');
+          }
+          else if (n) {
+            add_numering(file_out, &line_num, 'n');
+          }
+        }
+        if (e) {
+          replace_char(file_out, '\n');
+        }
+
+        if (v) {
+          show_nonprinting(file_out);
+        }
+
+        if (empty_flag < 2) {
+          printf("%s", file_out);
+        }
+
+        clean(file_out);
+      }
+      else {
+        break;
+      }
+    }
+    fclose(cat_file);
+  }
+  else {
+    fprintf(stderr, "cat: %s: No such file or directory\n", file_name);
+  }
+}
+
+void clean(char *str) {
+  int i = 0;
+  while (str[i] != '\0')
+  {
+    str[i] = '\0';
+    i++;
+  }
+}
+
+void get_flags(char *flags[10], int flags_count, int *e, int *n, int *b, int *t, int *s,
+               int *v) {
+  for (int i = 0; i < flags_count; i++) {
+    if (!strcmp(flags[i], "-e") || !strcmp(flags[i], "-E")) {
+      if (!strcmp(flags[i], "-e")) {
+        *v = 1;
+      }
+      *e = 1;
+    }
+    else if (!strcmp(flags[i], "-n") || !strcmp(flags[i], "--number")) {
+      *n = 1;
+    }
+    else if (!strcmp(flags[i], "-b") ||
+             !strcmp(flags[i], "--number-nonblank")) {
+      *b = 1;
+    }
+    else if (!strcmp(flags[i], "-t") || !strcmp(flags[i], "-T")) {
+      if (!strcmp(flags[i], "-t")) {
+        *v = 1;
+      }
+      *t = 1;
+    }
+    else if (!strcmp(flags[i], "-s") ||
+             !strcmp(flags[i], "--squeeze-blank")) {
+      *s = 1;
+    }
+    else if (!strcmp(flags[i], "-v")) {
+      *v = 1;
+    }
+  }
+}
+
+void squeeze_blank(char *file_out, int *empty_flag) {
+  if (file_out[0] == '\n') {
+    *empty_flag += 1;
+  }
+  else {
+    *empty_flag = 0;
+  }
+}
+
+void replace_char(char *file_out, char symb) {
+  char *ptr;
+  int f = 1;
+  switch (symb) {
+  case '\n':
+    ptr = strchr(file_out, '\n');
+    if (ptr) {
+      memset(ptr, '$', 1);
+      memset(++ptr, '\n', 1);
+    }
+    break;
+
+  case '\t':
+    while (ptr || f) {
+      f = 0;
+      ptr = strchr(file_out, '\t');
+      char tmp[N];
+      if (ptr) {
+        memset(ptr, '^', 1);
+        ptr++;
+        strcpy(tmp, ptr);
+        memset(ptr, 'I', 1);
+        ptr++;
+        strcpy(ptr, tmp);
+      }
+    }
+    break;
+  }
+}
+
+void add_numering(char *file_out, int *str_num, char mode) {
+  char tmp[N];
+  char num[100];
+  char *ptr = tmp;
+  if (!(file_out[0] == '\n') || mode == 'n') {
+    *str_num += 1;
+    sprintf(num, "%6.d\t", (*str_num));
+    strcpy(ptr, num);
+    ptr += strlen(num);
+  }
+  strcpy(ptr, file_out);
+  strcpy(file_out, tmp);
+}
+
+void show_nonprinting(char *file_out) {
+  char tmp[N] = {'\0'};
+  char *ptr = tmp;
+  int f = 0;
+  size_t i;
+  char *h = strchr(file_out, '\0');
+  if (*++h > 0) {
+    f = 1;
+  }
+  for (i = 0; i < strlen(file_out) + f * 3; i++) {
+    if (((int)file_out[i] <= 31 && (int)file_out[i] != 9 &&
+         (int)file_out[i] != 10) ||
+        (int)file_out[i] == 127) {
+      memset(ptr, '^', 1);
+      ptr++;
+      if ((int)file_out[i] == 127) {
+        memset(ptr, (char)(((int)file_out[i] + 64) & 127), 1);
+      }
+      else {
+        memset(ptr, (char)((int)file_out[i] + 64), 1);
+      }
+      ptr++;
+    }
+    else {
+      memset(ptr, file_out[i], 1);
+      ptr++;
+    }
+  }
+  strcpy(file_out, tmp);
+}
+
+void get_args(int argc, char *argv[argc], char *flags[10], char *files[100],
+              int *flags_counter, int *files_counter) {
+  int f_flag = 1;
+  for (int i = 1; i < argc; i++) {
+    if (f_flag & (!strcmp(argv[i], "-n") || !strcmp(argv[i], "-b") ||
+                  !strcmp(argv[i], "-e") || !strcmp(argv[i], "-v") ||
+                  !strcmp(argv[i], "-t") || !strcmp(argv[i], "-s") ||
+                  !strcmp(argv[i], "--number") || !strcmp(argv[i], "-T") ||
+                  !strcmp(argv[i], "-E") || !strcmp(argv[i], "--number-nonblank") ||
+                  !strcmp(argv[i], "--squeeze-blank"))) {
+      flags[*flags_counter] = argv[i];
+      *flags_counter += 1;
+    }
+    else {
+      files[*files_counter] = argv[i];
+      *files_counter += 1;
+      f_flag = 0;
+    }
+  }
+}
---
+++ C3_SimpleBashUtils/src/grep/s21_grep.c
@@ -0,0 +1,289 @@
+#include <ctype.h>
+#include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 1000
+
+void get_args(int argc, char *argv[argc], char flags[100], char *patterns[100],
+              char *files[100], int *j, int *k, int *l, int *free_arr,
+              int *arr_idx);
+void grep(char *file_name, char *pattern[100], char flags[100], int j,
+          int n_files, int ptrns);
+void flag_o(char *find_str, char *pattern[100], char *file_name, int i, int n,
+            int h, int num_of_str, int ptrns, int n_files);
+int find_regex(char *file_out, char *pattern, int i);
+void clean(char *var);
+
+int main(int argc, char *argv[]) {
+  char flags[100];
+  char *patterns[100];
+  char *files[100];
+  int j = 0;  // flags
+  int k = 0;  // patterns
+  int l = 0;  // files
+
+  int *arr = (int *)malloc(sizeof(int) * 100);
+  int yo = 0;
+  get_args(argc, argv, flags, patterns, files, &j, &k, &l, arr, &yo);
+
+  for (int i = 0; i < l; i++) {
+    grep(files[i], patterns, flags, j, l, k);
+  }
+
+  for (int i = 0; i < yo; i++) {
+    free(patterns[arr[i]]);
+  }
+
+  free(arr);
+
+  return 0;
+}
+
+void grep(char *file_name, char *pattern[100], char flags[100], int j,
+          int n_files, int ptrns) {
+  int count = 0;
+  char file_out[N] = {'\0'};
+  int is_pattern, count_of_in;
+  int l_printed = 0;
+  int num_of_str = 0;
+  FILE *cat_file = fopen(file_name, "r");
+  int v = 0;
+  int c = 0;
+  int i = 0;
+  int l = 0;
+  int n = 0;
+  int h = 0;
+  int s = 0;
+  int o = 0;
+
+  for (int u = 0; u < j; u++) {
+    if (flags[u] == 'v') {
+      v = 1;
+    } else if (flags[u] == 'c') {
+      c = 1;
+    } else if (flags[u] == 'i') {
+      i = 1;
+    } else if (flags[u] == 'l') {
+      l = 1;
+    } else if (flags[u] == 'n') {
+      n = 1;
+    } else if (flags[u] == 'h') {
+      h = 1;
+    } else if (flags[u] == 's') {
+      s = 1;
+    } else if (flags[u] == 'o') {
+      o = 1;
+    }
+  }
+
+  if (cat_file) {
+    while (!feof(cat_file)) {
+      num_of_str++;
+      fgets(file_out, 1000, cat_file);
+      if (file_out[0] != '\0') {
+        count_of_in = 0;
+        for (int m = 0; m < ptrns; m++) {
+          is_pattern = !find_regex(file_out, pattern[m], i);
+          if (is_pattern) {
+            count_of_in++;
+          }
+
+          if (c) {
+            if (v) {
+              if (!is_pattern && count_of_in == 0 && m == ptrns - 1) {
+                count++;
+                if (!l && !v) {
+                  break;
+                }
+              }
+            } else {
+              if (is_pattern) {
+                count++;
+                if (!l) {
+                  break;
+                }
+              }
+            }
+          }
+          if (l) {
+            if ((is_pattern && !v) ||
+                (v && !is_pattern && count_of_in == 0 && m == ptrns - 1)) {
+              l_printed = 1;
+              break;
+            }
+          }
+          if (is_pattern && !c && !v) {
+            if (n) {
+              if (!o) {
+                (n_files > 1 && !h)
+                    ? printf("%s:%d:%s", file_name, num_of_str, file_out)
+                    : printf("%d:%s", num_of_str, file_out);
+              } else {
+                flag_o(file_out, pattern, file_name, i, n, h, num_of_str, ptrns,
+                       n_files);
+              }
+            } else {
+              if (!o) {
+                (n_files > 1 && !h) ? printf("%s:%s", file_name, file_out)
+                                    : printf("%s", file_out);
+              } else {
+                flag_o(file_out, pattern, file_name, i, n, h, num_of_str, ptrns,
+                       n_files);
+              }
+            }
+            break;
+          }
+        }
+        if (l_printed) {
+          break;
+        }
+
+        if (!is_pattern && count_of_in == 0 && v && strlen(file_out) > 0 &&
+            !c) {
+          if (n) {
+            (n_files > 1 && !h)
+                ? printf("%s:%d:%s", file_name, num_of_str, file_out)
+                : printf("%d:%s", num_of_str, file_out);  // output for -v
+          } else {
+            (n_files > 1 && !h) ? printf("%s:%s", file_name, file_out)
+                                : printf("%s", file_out);  // output for -v
+          }
+        }
+
+        if (!o || v) {
+          if (((file_out[strlen(file_out) - 1] != '\n') && (is_pattern) && !c &&
+               !v) ||
+              (v && count_of_in == 0 &&
+               file_out[strlen(file_out) - 1] != '\n' && !c)) {
+            printf("\n");
+          }
+        }
+
+        clean(file_out);
+      }
+    }
+
+    if (c) {
+      (n_files > 1 && !h) ? printf("%s:%d\n", file_name, count)
+                          : printf("%d\n", count);
+    }
+    if (l_printed) {
+      printf("%s\n", file_name);
+    }
+
+    fclose(cat_file);
+  } else if (!s) {
+    fprintf(stderr, "grep: %s: No such file or directory\n", file_name);
+  }
+}
+
+void get_args(int argc, char *argv[argc], char flags[100], char *patterns[100],
+              char *files[100], int *j, int *k, int *l, int *free_arr,
+              int *arr_idx) {
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      int y = i;
+      for (size_t p = 1; p < strlen(argv[y]); p++) {
+        switch (argv[y][p]) {
+          case 'e':
+            i++;
+            y++;
+            patterns[*k] = argv[y];
+            *k += 1;
+            y--;
+            break;
+          case 'f':;
+            FILE *tmp_f = fopen(argv[++i], "r");
+            if (tmp_f) {
+              while (!feof(tmp_f)) {
+                patterns[*k] = malloc(sizeof(char) * N);
+                free_arr[*arr_idx] = *k;
+                *arr_idx += 1;
+                fgets(patterns[*k], N, tmp_f);
+                if (patterns[*k][strlen(patterns[*k]) - 1] == '\n') {
+                  patterns[*k][strlen(patterns[*k]) - 1] = '\0';
+                }
+                *k += 1;
+              }
+            }
+            fclose(tmp_f);
+            break;
+
+          default:
+            flags[*j] = argv[y][p];
+            *j += 1;
+            break;
+        }
+      }
+
+    } else {
+      if (*k == 0) {
+        patterns[*k] = argv[i];
+        *k += 1;
+      } else {
+        files[*l] = argv[i];
+        *l += 1;
+      }
+    }
+  }
+  // for (int i = 0; i < *l; i++) printf("%s ", files[i]);
+  // printf("\n");
+  // for (int i = 0; i < *k; i++) printf("%s ", patterns[i]);
+  // printf("\n");
+  // for (int i = 0; i < *j; i++) printf("%c ", flags[i]);
+  // printf("\n");
+}
+
+int find_regex(char *file_out, char *pattern, int i) {
+  regex_t re;
+  int val;
+  regcomp(&re, pattern, (i) ? REG_ICASE : REG_EXTENDED);
+  val = regexec(&re, file_out, 0, NULL, 0);
+  regfree(&re);
+  return val;
+}
+
+void clean(char *var) {
+  int i = 0;
+  while (var[i] != '\0') {
+    var[i] = '\0';
+    i++;
+  }
+}
+
+void flag_o(char *find_str, char *pattern[100], char *file_name, int i, int n,
+            int h, int num_of_str, int ptrns, int n_files) {
+  int n_printed = 0;
+  for (int l = 0; l < ptrns; l++) {
+    regex_t re;
+    char out[100] = {'\0'};
+    regmatch_t pmatch[100];
+    int status;
+    char *ps = find_str;
+
+    int eflag;
+    int len_out;
+
+    status = regcomp(&re, pattern[l], (i) ? REG_ICASE : REG_EXTENDED);
+    eflag = 0;
+
+    /* Найти все соответствия */
+    while ((status = regexec(&re, ps, 1, pmatch, eflag)) == 0) {
+      len_out = pmatch[0].rm_eo - pmatch[0].rm_so;
+      strncpy(out, ps + pmatch[0].rm_so, len_out);
+      if (n && !n_printed) {
+        (n_files > 1 && !h) ? printf("%s:%d:%s\n", file_name, num_of_str, out)
+                            : printf("%d:%s\n", num_of_str, out);
+        n_printed = 1;
+      } else {
+        (n_files > 1 && !h) ? printf("%s:%s\n", file_name, out)
+                            : printf("%s\n", out);
+      }
+      ps += pmatch[0].rm_eo;
+      eflag = REG_NOTBOL;
+    }
+
+    regfree(&re);
+  }
+}
